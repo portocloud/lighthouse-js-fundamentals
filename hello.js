1ab2abdc41ce199444bf6903f7b4aa16b083d218
Kurtis <kurtis@portocloud.com>
2016-10-14 18:23:27 +0000

    Adds functions exercises

---
+++ hello.js
@@ -0,0 +1,11 @@
+function sayHello() {
+  console.log("Hello, world")
+}
+sayHello()
+
+function sayHello(name) {
+  console.log("Hello, " + name)
+}
+sayHello("Caliban")
+sayHello("Miranda")
+sayHello("Ferdinand")
